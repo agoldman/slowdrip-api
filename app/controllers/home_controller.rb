054e44484981532be57de52354a1f76b90f68d08
April Goldman <aprileliza@gmail.com>
2018-12-29 19:56:30 -0800

    Adds a home page with jbuilder and installs devise.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+  	render :index, status: 202
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  unauthenticated do
+    root :to => 'home#index'
+  end
+  devise_for :users
+  authenticated do
+    root :to => 'users#profile'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
