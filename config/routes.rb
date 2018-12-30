3362eee6b0247d0a78720824079ad14bf5327729
April Goldman <aprileliza@gmail.com>
2018-12-30 10:03:21 -0800

    Sets up devise token authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
 	namespace :v1 do
-
-	  unauthenticated do
-		root :to => 'home#index'
-      end
-      devise_for :users
-      authenticated do
-		root :to => 'users#profile'
-      end
-	end
+	  mount_devise_token_auth_for 'User', at: 'auth'
+      root :to => 'home#index'
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
