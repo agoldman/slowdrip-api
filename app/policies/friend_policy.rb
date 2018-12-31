95782946016edb284b9ec3e1401c127c4c799a77
April Goldman <aprileliza@gmail.com>
2018-12-31 10:14:17 -0800

    Includes friend policy. Includes some minimal seed data.

---
+++ app/policies/friend_policy.rb
@@ -0,0 +1,12 @@
+class FriendPolicy
+  attr_reader :user, :friend
+
+  def initialize(user, friend)
+    @user = user
+    @friend = friend
+  end
+
+  def show?
+    @friend.friends.exists?(@user.id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#TODO: Create a bunch more users with Faker
+
+#Users
+april = User.create!(email: "april@gmail.com", password: "123asdf123")
+lloyd = User.create!(email: "lloyd@yahoo.com", password: "woofwoof12")
+adam = User.create!(email: "adam@snowflek.com", password: "snowfleklvr")
+
+#Friendships
+april.friends << lloyd
+adam.friends << lloyd
+
+#Droplets
+april.droplets.create!(content: "I'm having a good day [friends]", permission: "friends")
+april.droplets.create!(content: "I like Sightglass decaf [friends]", permission: "friends")
+april.droplets.create!(content: "My deepest secret [personal]", permission: "personal")
+april.droplets.create!(content: "My darkest secret [personal]", permission: "personal")
+
+lloyd.droplets.create!(content: "Woof woof woof [friends]", permission: "friends")
+lloyd.droplets.create!(content: "Bark bark bark [friends]", permission: "friends")
+lloyd.droplets.create!(content: "I can secretly talk [personal]", permission: "personal")
+lloyd.droplets.create!(content: "I know calculus [personal]", permission: "personal")
+
+adam.droplets.create!(content: "I live in San Francisco [friends]", permission: "friends")
+adam.droplets.create!(content: "I like music [friends]", permission: "friends")
+adam.droplets.create!(content: "I want to steal lloyd [personal]", permission: "personal")
+adam.droplets.create!(content: "My plot to steal lloyd is coming along [personal]", permission: "personal")
