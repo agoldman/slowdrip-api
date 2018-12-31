2d1fbb669e493ef354f4389684c9970c6576ea0b
April Goldman <aprileliza@gmail.com>
2018-12-30 16:02:02 -0800

    Adds index to droplets created at. We'll see if this helps with search.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_30_235104) do
+ActiveRecord::Schema.define(version: 2018_12_31_000041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_12_30_235104) do
     t.string "content", limit: 300
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_droplets_on_created_at"
     t.index ["user_id"], name: "index_droplets_on_user_id"
   end
 
