bfa5982e495f197da238e344962d04676bfed5e4
April Goldman <aprileliza@gmail.com>
2018-12-31 12:30:19 -0800

    Seeds droplets on the same day. Replaces schema.rb with structure.sql to support droping and rebuilding a pg db with enums.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 
 #TODO: Create a bunch more users with Faker
 
@@ -18,17 +14,38 @@ april.friends << lloyd
 adam.friends << lloyd
 
 #Droplets
-april.droplets.create!(content: "I'm having a good day [friends]", permission: "friends")
-april.droplets.create!(content: "I like Sightglass decaf [friends]", permission: "friends")
-april.droplets.create!(content: "My deepest secret [personal]", permission: "personal")
-april.droplets.create!(content: "My darkest secret [personal]", permission: "personal")
+april.droplets.create!(content: "I'm having a good day [friends]",
+                       permission: "friends")
+april.droplets.create!(content: "I like Sightglass decaf [friends]",
+                       permission: "friends",
+	                   created_at: Time.now - 1.year)
+april.droplets.create!(content: "My deepest secret [personal]",
+	                   permission: "personal",
+	                   created_at: Time.now - 1.day)
+april.droplets.create!(content: "My darkest secret [personal]",
+                       permission: "personal",
+                       created_at: Time.now - 2.years)
 
-lloyd.droplets.create!(content: "Woof woof woof [friends]", permission: "friends")
-lloyd.droplets.create!(content: "Bark bark bark [friends]", permission: "friends")
-lloyd.droplets.create!(content: "I can secretly talk [personal]", permission: "personal")
-lloyd.droplets.create!(content: "I know calculus [personal]", permission: "personal")
+lloyd.droplets.create!(content: "Woof woof woof [friends]",
+                       permission: "friends")
+lloyd.droplets.create!(content: "Bark bark bark [friends]",
+                       permission: "friends",
+                       created_at: Time.now - 1.year)
+lloyd.droplets.create!(content: "I can secretly talk [personal]",
+                       permission: "personal",
+                       created_at: Time.now - 1.day)
+lloyd.droplets.create!(content: "I know calculus [personal]",
+                       permission: "personal",
+                       created_at: Time.now - 2.years)
 
-adam.droplets.create!(content: "I live in San Francisco [friends]", permission: "friends")
-adam.droplets.create!(content: "I like music [friends]", permission: "friends")
-adam.droplets.create!(content: "I want to steal lloyd [personal]", permission: "personal")
-adam.droplets.create!(content: "My plot to steal lloyd is coming along [personal]", permission: "personal")
+adam.droplets.create!(content: "I live in San Francisco [friends]",
+                      permission: "friends")
+adam.droplets.create!(content: "I like music [friends]",
+                      permission: "friends",
+                      created_at: Time.now - 1.year)
+adam.droplets.create!(content: "I want to steal lloyd [personal]",
+                      permission: "personal",
+                      created_at: Time.now - 1.day)
+adam.droplets.create!(content: "My plot to steal lloyd is coming along [personal]",
+                      permission: "personal",
+                      created_at: Time.now - 2.years)
---
+++ db/structure.sql
@@ -0,0 +1,343 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: permission_level; Type: TYPE; Schema: public; Owner: -
+--
+
+CREATE TYPE public.permission_level AS ENUM (
+    'personal',
+    'friends'
+);
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.ar_internal_metadata (
+    key character varying NOT NULL,
+    value character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: droplets; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.droplets (
+    id bigint NOT NULL,
+    user_id bigint,
+    content character varying(300),
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL,
+    permission public.permission_level DEFAULT 'personal'::public.permission_level
+);
+
+
+--
+-- Name: droplets_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.droplets_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: droplets_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.droplets_id_seq OWNED BY public.droplets.id;
+
+
+--
+-- Name: friendships; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.friendships (
+    id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    friend_id bigint NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: friendships_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.friendships_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: friendships_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.friendships_id_seq OWNED BY public.friendships.id;
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id bigint NOT NULL,
+    provider character varying DEFAULT 'email'::character varying NOT NULL,
+    uid character varying DEFAULT ''::character varying NOT NULL,
+    encrypted_password character varying DEFAULT ''::character varying NOT NULL,
+    reset_password_token character varying,
+    reset_password_sent_at timestamp without time zone,
+    allow_password_change boolean DEFAULT false,
+    remember_created_at timestamp without time zone,
+    sign_in_count integer DEFAULT 0 NOT NULL,
+    current_sign_in_at timestamp without time zone,
+    last_sign_in_at timestamp without time zone,
+    current_sign_in_ip character varying,
+    last_sign_in_ip character varying,
+    confirmation_token character varying,
+    confirmed_at timestamp without time zone,
+    confirmation_sent_at timestamp without time zone,
+    unconfirmed_email character varying,
+    name character varying,
+    nickname character varying,
+    image character varying,
+    email character varying,
+    tokens json,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: droplets id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.droplets ALTER COLUMN id SET DEFAULT nextval('public.droplets_id_seq'::regclass);
+
+
+--
+-- Name: friendships id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.friendships ALTER COLUMN id SET DEFAULT nextval('public.friendships_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.ar_internal_metadata
+    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
+
+
+--
+-- Name: droplets droplets_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.droplets
+    ADD CONSTRAINT droplets_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: friendships friendships_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.friendships
+    ADD CONSTRAINT friendships_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: index_droplets_on_created_at; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_droplets_on_created_at ON public.droplets USING btree (created_at);
+
+
+--
+-- Name: index_droplets_on_permission; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_droplets_on_permission ON public.droplets USING btree (permission);
+
+
+--
+-- Name: index_droplets_on_user_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_droplets_on_user_id ON public.droplets USING btree (user_id);
+
+
+--
+-- Name: index_friendships_on_friend_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_friendships_on_friend_id ON public.friendships USING btree (friend_id);
+
+
+--
+-- Name: index_friendships_on_user_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_friendships_on_user_id ON public.friendships USING btree (user_id);
+
+
+--
+-- Name: index_users_on_confirmation_token; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_users_on_confirmation_token ON public.users USING btree (confirmation_token);
+
+
+--
+-- Name: index_users_on_email; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_users_on_email ON public.users USING btree (email);
+
+
+--
+-- Name: index_users_on_reset_password_token; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_users_on_reset_password_token ON public.users USING btree (reset_password_token);
+
+
+--
+-- Name: index_users_on_uid_and_provider; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_users_on_uid_and_provider ON public.users USING btree (uid, provider);
+
+
+--
+-- Name: droplets fk_rails_15a7967a65; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.droplets
+    ADD CONSTRAINT fk_rails_15a7967a65 FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: friendships fk_rails_d78dc9c7fd; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.friendships
+    ADD CONSTRAINT fk_rails_d78dc9c7fd FOREIGN KEY (friend_id) REFERENCES public.users(id);
+
+
+--
+-- Name: friendships fk_rails_e3733b59b7; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.friendships
+    ADD CONSTRAINT fk_rails_e3733b59b7 FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+SET search_path TO "$user", public;
+
+INSERT INTO "schema_migrations" (version) VALUES
+('20181230033010'),
+('20181230173145'),
+('20181230173242'),
+('20181230194241'),
+('20181230195543'),
+('20181230235104'),
+('20181231000041'),
+('20181231002323'),
+('20181231030124'),
+('20181231030542'),
+('20181231032853');
+
+
