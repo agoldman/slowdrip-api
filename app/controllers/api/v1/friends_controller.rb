3ea5e3b9ff1403c84a709968d9e6102dad6acc89
April Goldman <aprileliza@gmail.com>
2019-01-05 00:03:25 -0800

    Debugs friends index to show all friends friendly droplets for today.

--- app/controllers/api/v1/friends_controller.rb
+++ app/controllers/api/v1/friends_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::FriendsController < ApplicationController
   before_action :authenticate_api_v1_user!
 
+  #TODO make this not an n+1 query.
   def index
   	@user = User.find(params[:user_id])
  	@friends = @user.friends.includes(:droplets)
+ 	@friends.each do |friend|
+      nd = friend.droplets.select do |droplet|
+            (droplet.permission == "friends") &&
+            (droplet.created_at.month == Time.zone.now.month) &&
+            (droplet.created_at.day == Time.zone.now.day)
+      end
+      friend.droplets = nd
+ 	end
   end
 
 end
