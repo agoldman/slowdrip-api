12de557860cfa277af3ac031531a9b5b598d57d5
April Goldman <aprileliza@gmail.com>
2019-01-12 10:39:37 -0800

    Makes record not found error response the same as the devise error response format.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,18 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   private
 
   def pundit_user
     current_api_v1_user
   end
+
+  def record_not_found(e)
+    @response = { success: false, errors: [e] }
+    render json: @response, status: :not_found
+  end
+
 end
 
